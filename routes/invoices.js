15e3d0cd74c9fd24f6c6558a039004f964e78bdb
unknown <exgin3538@yahoo.com>
2020-05-19 20:49:06 -0400

    finish RESTful routes for invoices

--- routes/invoices.js
+++ routes/invoices.js
@@ -12,4 +12,74 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const results = await db.query(
+      `SELECT i.id, i.comp_code, i.amt, i.paid, i.add_date, i.paid_date, c.name, c.description FROM invoices AS i INNER JOIN companies AS c ON (i.comp_code = c.code) WHERE id = $1`,
+      [id]
+    );
+    if (results.rows.length === 0) {
+      throw new ExpressError(`Cannot find invoice id: ${id}`, 404);
+    }
+    // get our usual data
+    let data = results.rows[0];
+    // extract the data we need
+    let finalData = {
+      id: data.id,
+      company: {
+        code: data.comp_code,
+        name: data.name,
+        description: data.description,
+      },
+      amt: data.amt,
+      paid: data.paid,
+      add_date: data.add_date,
+      paid_date: data.paid_date,
+    };
+
+    return res.json({ invoice: finalData });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const { comp_code, amt } = req.body;
+    const results = await db.query(`INSERT INTO invoices (comp_code, amt) VALUES ($1, $2) RETURNING id, comp_code, amt, paid, add_date, paid_date`, [comp_code, amt]);
+    return res.json({ invoice: results.rows[0] });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+router.patch('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { amt } = req.body;
+    const results = await db.query(`UPDATE invoices SET amt = $1 WHERE id = $2 RETURNING id, comp_code, amt, paid, add_date, paid_date`, [amt, id]);
+
+    if (results.rows.length === 0) {
+      throw new ExpressError(`Invoice cannot be found :${id}`);
+    }
+    return res.json({ invoice: results.rows[0] });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const results = await db.query(`DELETE FROM invoices WHERE id = $1 RETURNING id`, [id]);
+
+    if (results.rows.length === 0) {
+      throw new ExpressError(`Cannot find invoice: ${id}, to delete`, 404);
+    }
+    return res.json({ message: 'Invoice deleted' });
+  } catch (error) {
+    return next(error);
+  }
+});
 module.exports = router;
