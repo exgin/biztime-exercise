a599d2c65b019246eab80ec8a709b5e590d46bad
unknown <exgin3538@yahoo.com>
2020-05-19 21:06:54 -0400

    adding tests to company

---
+++ routes/companies.test.js
@@ -0,0 +1,27 @@
+process.env.NODE_ENV = 'test';
+
+const request = require('supertest');
+const app = require('../app');
+const db = require('../db');
+
+let testData;
+beforeEach(async () => {
+  const result = await db.query(`INSERT INTO companies (code, name, description) VALUES ('amd', 'Advanced Microparts', 'Computer Parts') RETURNING code, name, description`);
+  testData = result.rows[0];
+});
+
+afterEach(async () => {
+  await db.query(`DELETE FROM companies`);
+});
+
+afterAll(async () => {
+  await db.end();
+});
+
+describe('GET /companies', () => {
+  test('Get a list of all companies', async () => {
+    const res = await request(app).get('/companies');
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual({ companies: [res.rows] });
+  });
+});
